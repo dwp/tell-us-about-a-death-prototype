dac44210b89810ac74b9963bebdf5221fdf8c464
Richard Young <richard.young3@dwp.gsi.gov.uk>
2018-01-30 15:54:16 +0000

    Add comments to routes

--- app/routes.js
+++ app/routes.js
@@ -8,7 +8,7 @@ router.get('/', function (req, res) {
 
 // Add your routes here - above the module.exports line
 
-
+// Routes for Sprint 3 Tin prototype
 router.post('/protos/tuaad-sprint-3/tuaad-tin/2-details-deceased', function (req, res) {
   if (req.body['first-name-2']=='Alice'||req.body['first-name-2']=='alice'){ 
     res.redirect('/protos/tuaad-sprint-3/tuaad-tin/5-complete')
@@ -30,5 +30,6 @@ router.post('/protos/tuaad-sprint-3/tuaad-tin/2b-details-deceased', function (re
   }
 
 })
+// End routes for Sprint 3 Tin prototype
 
 module.exports = router
