d2eb7ab95a4a9609e18a25653e8d90251a3179af
Richard Young <richard.young3@dwp.gsi.gov.uk>
2018-02-05 12:59:00 +0000

    Add 'gold' routes

--- app/routes.js
+++ app/routes.js
@@ -8,7 +8,7 @@ router.get('/', function (req, res) {
 
 // Add your routes here - above the module.exports line
 
-// Routes for Sprint 3 Tin prototype
+// Routes for Tin prototype
 router.post('/protos/tuaad-sprint-3/tuaad-tin/2-details-deceased', function (req, res) {
   if (req.body['first-name-2']=='Alice'||req.body['first-name-2']=='alice'){ 
     res.redirect('/protos/tuaad-sprint-3/tuaad-tin/5-complete')
@@ -30,6 +30,30 @@ router.post('/protos/tuaad-sprint-3/tuaad-tin/2b-details-deceased', function (re
   }
 
 })
-// End routes for Sprint 3 Tin prototype
+// End routes for Tin prototype
+
+// Routes for Gold prototype
+router.post('/protos/tuaad-gold/2-details-deceased', function (req, res) {
+  if (req.body['first-name-2']=='Alice'||req.body['first-name-2']=='alice'){ 
+    res.redirect('/protos/tuaad-gold/5-complete')
+  } else if(req.body['first-name-2']=='Jane'||req.body['first-name-2']=='jane') { 
+    res.redirect('/protos/tuaad-gold/3-details-notifier')
+  } else {
+    res.redirect('/protos/tuaad-gold/2b-details-deceased')
+  }
+
+})
+
+router.post('/protos/tuaad-gold/2b-details-deceased', function (req, res) {
+  if (req.body['first-name-2']=='Alice'||req.body['first-name-2']=='alice'){ 
+    res.redirect('/protos/tuaad-gold/5-complete')
+  } else if(req.body['first-name-2']=='Jane'||req.body['first-name-2']=='jane') { 
+    res.redirect('/protos/tuaad-gold/3-details-notifier')
+  } else {
+    res.redirect('/protos/tuaad-gold/2b-details-deceased')
+  }
+
+})
+// End routes for Gold prototype
 
 module.exports = router
