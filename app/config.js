53d55b257a94c63e4e37c3097dc02bcec24ef622
Richard Young <richard.young3@dwp.gsi.gov.uk>
2018-03-02 16:53:36 +0000

    Change serviceName

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Tell us about a death',
+  serviceName: 'Prototype service',
 
   // Default port that prototype runs on
   port: '3000',
