2d78b05fcf74d0aa7cbdeecc8f78c6cefe987f9b
Mike Shaw <mike.shaw@dwp.gsi.gov.uk>
2018-05-08 14:59:06 +0100

    Various fixes

--- app/routes.js
+++ app/routes.js
@@ -10,50 +10,46 @@ router.get('/', function (req, res) {
 
 // Routes for Tin prototype
 router.post('/protos/tuaad-sprint-3/tuaad-tin/2-details-deceased', function (req, res) {
-  if (req.body['first-name-2']=='Alice'||req.body['first-name-2']=='alice'){ 
+  if (req.body['first-name-2'] === 'Alice' || req.body['first-name-2'] === 'alice') {
     res.redirect('/protos/tuaad-sprint-3/tuaad-tin/5-complete')
-  } else if(req.body['first-name-2']=='Jane'||req.body['first-name-2']=='jane') { 
+  } else if (req.body['first-name-2'] === 'Jane' || req.body['first-name-2'] === 'jane') {
     res.redirect('/protos/tuaad-sprint-3/tuaad-tin/3-details-notifier')
   } else {
     res.redirect('/protos/tuaad-sprint-3/tuaad-tin/2b-details-deceased')
   }
-
 })
 
 router.post('/protos/tuaad-sprint-3/tuaad-tin/2b-details-deceased', function (req, res) {
-  if (req.body['first-name-2']=='Alice'||req.body['first-name-2']=='alice'){ 
+  if (req.body['first-name-2'] === 'Alice' || req.body['first-name-2'] === 'alice') {
     res.redirect('/protos/tuaad-sprint-3/tuaad-tin/5-complete')
-  } else if(req.body['first-name-2']=='Jane'||req.body['first-name-2']=='jane') { 
+  } else if (req.body['first-name-2'] === 'Jane' || req.body['first-name-2'] === 'jane') {
     res.redirect('/protos/tuaad-sprint-3/tuaad-tin/3-details-notifier')
   } else {
     res.redirect('/protos/tuaad-sprint-3/tuaad-tin/2b-details-deceased')
   }
-
 })
 // End routes for Tin prototype
 
 // Routes for Gold prototype
-//router.post('/protos/tuaad-gold/2-details-deceased', function (req, res) {
- // if (req.body['first-name-2']=='Alice'||req.body['first-name-2']=='alice'){ 
-  //  res.redirect('/protos/tuaad-gold/3-details-notifier')
- // } else if(req.body['first-name-2']=='Jane'||req.body['first-name-2']=='jane') { 
- //   res.redirect('/protos/tuaad-gold/3-details-notifier')
- // } else {
-//    res.redirect('/protos/tuaad-gold/2b-details-deceased')
- // }
+// router.post('/protos/tuaad-gold/2-details-deceased', function (req, res) {
+//   if (req.body['first-name-2']=='Alice'||req.body['first-name-2']=='alice'){
+//     res.redirect('/protos/tuaad-gold/3-details-notifier')
+//   } else if(req.body['first-name-2']=='Jane'||req.body['first-name-2']=='jane') {
+//     res.redirect('/protos/tuaad-gold/3-details-notifier')
+//   } else {
+//     res.redirect('/protos/tuaad-gold/2b-details-deceased')
+//   }
+// })
 
-//})
-
-//router.post('/protos/tuaad-gold/2b-details-deceased', function (req, res) {
-//  if (req.body['first-name-2']=='Alice'||req.body['first-name-2']=='alice'){ 
-//    res.redirect('/protos/tuaad-gold/3-details-notifier')
-//  } else if(req.body['first-name-2']=='Jane'||req.body['first-name-2']=='jane') { 
-//    res.redirect('/protos/tuaad-gold/3-details-notifier')
-//  } else {
-//    res.redirect('/protos/tuaad-gold/2b-details-deceased')
-//  }
-
-//})
+// router.post('/protos/tuaad-gold/2b-details-deceased', function (req, res) {
+//   if (req.body['first-name-2']=='Alice'||req.body['first-name-2']=='alice') {
+//     res.redirect('/protos/tuaad-gold/3-details-notifier')
+//   } else if(req.body['first-name-2']=='Jane'||req.body['first-name-2']=='jane') {
+//     res.redirect('/protos/tuaad-gold/3-details-notifier')
+//   } else {
+//     res.redirect('/protos/tuaad-gold/2b-details-deceased')
+//   }
+// })
 // End routes for Gold prototype
 
 module.exports = router
